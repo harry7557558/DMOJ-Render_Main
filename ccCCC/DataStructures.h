f718c3a7c0263d0598a345b117149fe848fcae54
Harry Chen <44143868+Harry7557558@users.noreply.github.com>
2021-02-07 23:13:51 -0500

    :disjoint set:

--- ccCCC/DataStructures.h
+++ ccCCC/DataStructures.h
@@ -1,6 +1,11 @@
 #error Header "DataStructures.h" include.
 
 
+#define MAXN 1000000
+int N;
+
+
+
 // binary indexed tree / Fenwick tree
 int BIT[MAXN + 1];  // all elements are zero at first
 void updateBIT(int i, int d) {  // increase the ith element by d
@@ -18,7 +23,7 @@ int getsumBIT(int i) {  // return the sum from 0 to i, inclusive
 // segment tree
 struct segtree {
 	int a, b;  // [a,b)
-	datatype val;  // sum/prod
+	datatype val;  // sum/prod of int/mat/algebra structure
 	segtree *l, *r;  // children
 } R;
 void initTree(segtree *R, int a, int b) {
@@ -26,7 +31,7 @@ void initTree(segtree *R, int a, int b) {
 	int c = (a + b) / 2;
 	if (c == a) {
 		R->l = R->r = 0;
-		R->val = GlobalArray[a];
+		R->val = Arr[a];
 	}
 	else {
 		R->l = new segtree; initTree(R->l, a, c);
@@ -39,4 +44,33 @@ datatype queryTree(const segtree *R, int a, int b) {  // [a,b)
 	if (R->a >= b || R->b <= a) return unit_element;  // completely outside the interval
 	return queryTree(R->r, a, b) + queryTree(R->l, a, b);  // sum or prod
 }
+
+
+// disjoint set
+int parent[MAXN];  // all initialize to -1
+uint8_t rank[MAXN];  // all initialize to 0
+int findRepresentative(int i) {
+	if (parent[i] == -1) return i;
+	else {
+		int ans = findRepresentative(parent[i]);
+		parent[i] = ans;
+		return ans;
+	}
+}
+bool unionSet(int i, int j) {
+	int i_rep = findRepresentative(i);
+	int j_rep = findRepresentative(j);
+	if (i_rep == j_rep) return false;
+#if 0
+	parent[i_rep] = j_rep;
+#else
+	if (rank[i_rep] < rank[j_rep])
+		parent[i_rep] = j_rep;
+	else if (rank[i_rep] > rank[j_rep])
+		parent[j_rep] = i_rep;
+	else parent[j_rep] = i_rep, rank[i_rep]++;
+#endif
+	return true;
+}
+
 
